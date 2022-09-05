d762312321e76cf033f4e1f972aec7d85bcc39e2
Cascaval Nadejda <cascavalnadejda@gmail.com>
2022-09-05 20:12:25 +0300

    Четвертая задача

---
+++ Ex004/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i <= n; i+=2)
+{
+if (i == n || i == n - 1)
+    Console.Write(i);
+else
+    Console.Write(i + ", ");
+}
