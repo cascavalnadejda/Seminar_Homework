d6fd561f77f355c9bacc16eeefe483583b8d1810
Cascaval Nadejda <cascavalnadejda@gmail.com>
2022-09-05 19:55:23 +0300

    Вторая задача

---
+++ Ex002/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine(max);
+
