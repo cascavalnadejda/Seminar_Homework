03d3a7789d32c898b9b8dac8fee5a0ad6f9a2ab2
Cascaval Nadejda <cascavalnadejda@gmail.com>
2022-09-08 15:43:50 +0300

    Задача 13

---
+++ Ex006/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+    if (n < 99)
+        Console.WriteLine("Третья цифра отсутствует");
+    else
+        Console.WriteLine(Convert.ToString(n)[2]);
