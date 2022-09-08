c2f4229d500e8c4d61b5584afd2f47fc216224d0
Cascaval Nadejda <cascavalnadejda@gmail.com>
2022-09-08 15:44:12 +0300

    Задача 15

---
+++ Ex007/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным
+Console.Write("Input number of the day between 1 and 7: ");
+int n = Convert.ToInt32(Console.ReadLine());
+    if (n == 1)
+    Console.WriteLine("no");
+    if (n == 2) 
+    Console.WriteLine("no");
+    if (n == 3)
+    Console.WriteLine("no");
+    if (n == 4)
+    Console.WriteLine("no");
+    if (n == 5)
+    Console.WriteLine("no");
+    if (n == 6)
+    Console.WriteLine("yes");
+    if (n == 7)
+    Console.WriteLine("yes");
