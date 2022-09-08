9a9b7aa9b8c5bd1ae5ac825aa852a96d47857754
Cascaval Nadejda <cascavalnadejda@gmail.com>
2022-09-08 12:59:17 +0300

    Ex005

---
+++ Ex005/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int GenerateRandomNumbers()
+{
+    int n = new Random().Next(100, 1000);
+    return n;
+}
+int n = n % 100;
+Console.Writeline(a);
