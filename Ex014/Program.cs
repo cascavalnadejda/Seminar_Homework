590b9df7c127343f2f6153fa9389699223b6c495
Cascaval Nadejda <cascavalnadejda@gmail.com>
2022-09-17 12:57:27 +0300

    Задача 34

---
+++ Ex014/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+int SearchNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] %2 == 0)
+            count++;
+    }
+    return count;
+}
+int[] array = new int[4];
+InputArray(array);
+Console.WriteLine("Исходный массив данных: [" + string.Join(", ", array) + "]");
+Console.WriteLine("Количество четных чисел в массиве: " + SearchNumbers(array));
+
+
