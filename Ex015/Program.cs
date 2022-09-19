e482137b7c392a309126b33511414a856cccbfec
Cascaval Nadejda <cascavalnadejda@gmail.com>
2022-09-19 13:34:13 +0300

    Задача 36

---
+++ Ex015/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 6);
+}
+int Summa(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+        sum += array[i];
+    return sum;
+}
+int[] array = new int[5];
+InputArray(array);
+Console.WriteLine("Исходный массив данных: [" + string.Join(", ", array) + "]");
+Console.WriteLine("Сумма: " + Summa(array));
+
+
+
+
