81eb9bf1c29c2ee4bc4988bebe1d10d0b7287c60
Cascaval Nadejda <cascavalnadejda@gmail.com>
2022-09-21 17:21:45 +0300

    Задача 41

---
+++ Ex017/Program.cs
@@ -0,0 +1,15 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//     0, 7, 8, -2, -2 -> 2
+
+int M = new Random().Next(1, 10);
+int count = 0;
+Console.WriteLine("Введите числа: ");
+for (int i = 1; i <= M; i++)
+{
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num > 0) count++; 
+}
+Console.WriteLine($"количество элементов больше нуля = {count}");
+
+
+
